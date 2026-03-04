496cde5db1772823377e55625efc70d750e8bde4
Omar-cdm <omarjunior052202@gmail.com>
2026-03-04 00:16:06 -0500

    Inicio de migración a React

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,10 @@
+import  ButtonGithub  from './component/BtnGithub.tsx'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Min-Game</h1>
+            <ButtonGithub />
+        </header>
+    )
+};
