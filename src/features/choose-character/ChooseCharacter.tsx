e9885d4eec172ed0f203374d52057f6d76c3019f
Omar-cdm <omarjunior052202@gmail.com>
2026-03-07 23:51:48 -0500

    Objetos traidos de la api y renderizados en elementos card

--- src/features/choose-character/ChooseCharacter.tsx
+++ src/features/choose-character/ChooseCharacter.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import { CreateCard } from "./component/CardCharacter.tsx"
 
-export const SectionChooseCharacter: React.FC<{initGame: () => void}> = ({ initGame }) => {
+export const SectionChooseCharacter: React.FC<{initGame: (character: string) => void, characters: never[]}> = ({ initGame, characters }) => {
+    const [character, setCharacter] = useState("")
+    const charactersArray = characters
+
+    function handlePlay() {
+        initGame(character)
+        console.log(character)
+    }
+
     return (
        <section className="h-dvh flex flex-col justify-center items-center seccion-elegir-personaje">
             <h1 className=" text-center text-2xl text-sky-100 font-bold mb-4 sm:text-3xl">Choose Your Favorite Character</h1>
             <div className="w-full h-1/2 overflow-auto px-4 mb-4 grid grid-cols-2 gap-4 sm:grid-cols-4 container">
-                <CreateCard image="Homero" title="homero" />
+                {
+                    charactersArray.map((card: any) => {return <CreateCard key={card.id} image={card.portrait_path} title={card.name} selectCharacter={setCharacter} />})
+                }
             </div>
-            <button className="bg-yellow-300 text-xl font-bold w-32 min-h-10 rounded-md transition delay-110 duration-400 ease hover:bg-yellow-400 hover:scale-102 hover:shadow-xl hover:shadow-yellow-400/30 hover:cursor-pointer jugar" onClick={initGame}>Play</button>
+            <button className="bg-yellow-300 text-xl font-bold w-32 min-h-10 rounded-md transition delay-110 duration-400 ease hover:bg-yellow-400 hover:scale-102 hover:shadow-xl hover:shadow-yellow-400/30 hover:cursor-pointer jugar" onClick={handlePlay}>Play</button>
         </section> 
     )
 }
--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { SectionHero } from "../../features/hero/Hero.tsx"
 import { SectionChooseCharacter } from '../../features/choose-character/ChooseCharacter.tsx'
 import { SectionGuessPhrase } from '../../features/guess-phase/GuessPhrase.tsx'
 
 export default function Main() {
     const [section, setSection] = useState('hero')
+    const [character, setCharacter] = useState('')
+    const [characters, setCharacters] = useState([])
+
+    function filterCharacter(characters: any) {
+        setCharacters(characters.filter((objeto: any) => objeto.phrases != "" && objeto.id != 10))
+    }
+
+    useEffect(() => {
+        const request = new Request("https://thesimpsonsapi.com/api/characters")
+        fetch(request)
+            .then(res => res.json())
+            .then(data => filterCharacter(data.results))
+
+    }, [])
+
+    function initGame(character: string) {
+        setSection('guess')
+        setCharacter(character)
+    }
 
     return (
         <main>
             { section === 'hero' && <SectionHero playGame={() => { setSection('choose') }}/> }
-            { section === 'choose' && <SectionChooseCharacter initGame={() => { setSection('guess') }}/> }
-            { section === 'guess' && <SectionGuessPhrase personaje='Homero' /> }
+            { section === 'choose' && <SectionChooseCharacter initGame={initGame} characters={characters}/> }
+            { section === 'guess' && <SectionGuessPhrase personaje={character} /> }
         </main>
     )
 }
